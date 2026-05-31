8e2154c571a42b77051a2a2384bf82dc55bdb829
fiinytid <arifiinytid@gmail.com>
2026-05-31 15:27:38 +0800

    fix: update dependencies for edge runtime

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,15 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
-const isPublicRoute = createRouteMatcher(['/', '/login(.*)']); // sesuaikan dengan rute kamu
+const isPublicRoute = createRouteMatcher(['/', '/login(.*)']);
 
 export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect();
+  const { userId } = auth();
+
+  // Jika bukan rute publik dan user belum login, arahkan ke rute /login
+  if (!userId && !isPublicRoute(request)) {
+    const loginUrl = new URL('/login', request.url);
+    return NextResponse.redirect(loginUrl);
   }
 });
 
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  transpilePackages: ['@clerk/nextjs', '@clerk/shared'],
   /* config options here */
 };
 
