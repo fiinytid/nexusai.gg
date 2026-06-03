f9877b7f2d2e1579f3329d4046f0d4565dd1e1bd
fiinytid <arifiinytid@gmail.com>
2026-06-03 13:18:36 +0800

    fix: update website for fixed all error

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,6 @@ const nextConfig: NextConfig = {
   transpilePackages: ['@clerk/nextjs', '@clerk/shared'],
   /* config options here */
   
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/js/:path*',
-          destination: '/404',
-        },
-      ],
-    }
-  },
 };
 
 export default nextConfig;
